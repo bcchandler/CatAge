59f890f56178e074a897c5f0147111f53c305dc8
bcchandler <bcchandler@gmail.com>
2019-07-09 21:31:31 -0400

    initial commit

--- CatAge/ViewController.swift
+++ CatAge/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ageInput: UITextField!
+    
+    @IBOutlet weak var ageResult: UILabel!
+    
+    @IBAction func calculateAgeButton(_ sender: Any) {
+        if let age = ageInput.text {
+            if let intAge = Int(age) {
+                let catAge = intAge * 7
+                ageResult.text = "Your cat is " + String(catAge) + " years old."
+            }
+        }
+    }
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
